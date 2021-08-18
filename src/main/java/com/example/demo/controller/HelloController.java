9310ec6ac027f5b5bda1de22096682fc76afe970
Jason Hsiao <crazyjabet@gmail.com>
2021-08-18 23:37:59 +0800

    test

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,14 +1,18 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Health;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     @GetMapping("/health")
     public String greeting() {
+        log.info("hello");
+        System.err.println("====== Error hello ======");
         return new Health().getHealthCheckPass();
     }
 
--- src/main/java/com/example/demo/model/Health.java
+++ src/main/java/com/example/demo/model/Health.java
@@ -1,10 +1,8 @@
 package com.example.demo.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class Health {
 
     private String healthCheckPass = "healthCheckPass";
